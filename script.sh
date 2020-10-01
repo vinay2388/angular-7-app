51c3e9fe22f73f9780be5dd59b377209bac34d46
vinay.reddy <vinay.reddy@stackaero.com>
2020-10-01 20:43:36 +0530

    script updated

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@ if [[ -z "${username}" && -z "${password}" ]]; then
   gitUrl=$giturl
 fi
 echo $gitUrl
+
 git clone ${gitUrl} /opt/apps/angular
 npm install -g @angular/cli
 cp /opt/apps/angular/start.sh /opt/apps
